2b49733b6aa5c311d80e173de2f63a1ce72ac7a3
manasesortez <manasesortez1234@hotmail.com>
2021-11-06 13:21:04 -0600

    SQLite

---
+++ app/src/main/java/com/amtodev/bitcoinconvert/Add_Contact.java
@@ -0,0 +1,14 @@
+package com.amtodev.bitcoinconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+    }
+}
